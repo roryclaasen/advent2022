d5e6b008922b610db2c64e1f9b0a16bea0560c50
Rory Claasen <rory@xbox.com>
2022-11-23 15:54:26 +0000

    Adds Kurukuru

--- Advent2022/Day1/Program.cs
+++ Advent2022/Day1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Kurukuru;
+
+Spinner.Start("Part 1", spinner =>
+{
+    Thread.Sleep(500);
+    spinner.Succeed("The answer is: 13");
+});
+
+Spinner.Start("Part 2", spinner =>
+{
+    Thread.Sleep(500);
+    spinner.Fail("Something went wrong!");
+});
