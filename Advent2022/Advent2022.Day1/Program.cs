70c5e826bacc2f7298ed87a714701755a04f0996
Rory Claasen <rory@xbox.com>
2022-12-01 09:28:21 +0000

    Adds Day1

--- Advent2022/Advent2022.Day1/Program.cs
+++ Advent2022/Advent2022.Day1/Program.cs
@@ -2,16 +2,43 @@ using Advent2022.Shared;
 using Kurukuru;
 
 var inputReader = new InputReader(typeof(Program).Assembly);
-var input = inputReader.ReadFile("input.txt");
+var input = ParseInput(inputReader.ReadFile("input.txt"));
 
 Spinner.Start("Part 1", spinner =>
 {
-    Thread.Sleep(500);
-    spinner.Succeed("The answer is: 13");
+    var elfs = input.ToArray();
+    var max = elfs[0].Calories.Sum();
+    var num = 0;
+    for (var i = 1; i < elfs.Length; i++)
+    {
+        var elf = elfs[i];
+        var cal = elf.Calories.Sum();
+        if (cal > max)
+        {
+            max = cal;
+            num = i;
+        }
+    }
+
+    spinner.Succeed($"The answer is: {max}");
 });
 
 Spinner.Start("Part 2", spinner =>
 {
-    Thread.Sleep(500);
-    spinner.Fail("Something went wrong!");
+    var total = input
+        .Select(e => e.Calories.Sum())
+        .Order()
+        .TakeLast(3)
+        .Sum();
+    spinner.Succeed($"The answer is: {total}");
 });
+
+IEnumerable<Elf> ParseInput(string input)
+{
+    foreach (var section in input.Split(Environment.NewLine + Environment.NewLine))
+    {
+        yield return new Elf(section.Split(Environment.NewLine).Select(int.Parse));
+    }
+}
+
+record Elf(IEnumerable<int> Calories);
